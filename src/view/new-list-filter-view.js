d1320d7ff2e4059b4f700beb185d0f252df2dbb0
Kolyada Danil <kd_kein@mail.ru>
2024-10-21 08:40:46 +1000

    Изменил верстку компонента формы и добавил кнопку новой точки в компоненты

--- src/view/new-list-filter-view.js
+++ src/view/new-list-filter-view.js
@@ -1,6 +1,6 @@
 import AbstractView from '../framework/view/abstract-view';
 
-function createNewFilterItemTemplate(filter, isChecked) {
+function createNewFilterItemTemplate(filter, currentFilterType) {
   const {type, count} = filter;
 
   return `<div class="trip-filters__filter">
@@ -11,29 +11,45 @@ function createNewFilterItemTemplate(filter, isChecked) {
               name="trip-filter"
               value="${ type }"
               ${ count === 0 ? 'disabled' : '' }
-              ${ isChecked ? 'checked' : '' }>
+              ${ currentFilterType === type ? 'checked' : '' }>
             <label class="trip-filters__filter-label" for="filter-${ type }">${ type }</label>
           </div>`;
 }
 
-function createNewListFilterTemplate (filterItems) {
-  const filterItemsTemplate = filterItems.map((filter, index) => createNewFilterItemTemplate(filter, index === 0)).join('');
-  return `<form class="trip-filters" action="#" method="get">
-            ${filterItemsTemplate}
-            <button class="visually-hidden" type="submit">Accept filter</button>
-          </form>`;
+function createNewListFilterTemplate (filterItems, currentFilterType) {
+  const filterItemsTemplate = filterItems.map((filter) => createNewFilterItemTemplate(filter, currentFilterType)).join('');
+  return `<div class="trip-main__trip-controls  trip-controls">
+            <div class="trip-controls__filters">
+              <h2 class="visually-hidden">Filter events</h2>
+              <form class="trip-filters" action="#" method="get">
+                ${filterItemsTemplate}
+                <button class="visually-hidden" type="submit">Accept filter</button>
+              </form>
+            </div>
+          </div>`;
 }
 
 export default class NewListFilterView extends AbstractView {
 
   #filters = null;
+  #handleFilterTypeChange = null;
+  #currentFilter = null;
 
-  constructor({ filters }) {
+  constructor({ filters, currentFilterType, onFilterTypeChange }) {
     super();
     this.#filters = filters;
+    this.#currentFilter = currentFilterType;
+    this.#handleFilterTypeChange = onFilterTypeChange;
+
+    this.element.addEventListener('change', (event) => this.#filterTypeChangeHandler(event));
   }
 
   get template() {
-    return createNewListFilterTemplate(this.#filters);
+    return createNewListFilterTemplate(this.#filters, this.#currentFilter);
   }
+
+  #filterTypeChangeHandler = (event) => {
+    event.preventDefault();
+    this.#handleFilterTypeChange();
+  };
 }
