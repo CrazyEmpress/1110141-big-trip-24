a735f570ca934a8082ed6284688386bdc1d08c6a
Kolyada Danil <kd_kein@mail.ru>
2024-10-21 10:30:16 +1000

    Добавил модель фильтра

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import TripsPresenter from './presenter/trip-presenter';
 import EventsModel from './model/events-model';
+import FilterModel from './model/filter-model.js';
 
 const eventsModel = new EventsModel();
+const filterModel = new FilterModel();
 const tripsPresenter = new TripsPresenter({eventsModel});
 
 
